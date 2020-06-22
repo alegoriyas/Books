f04b40e2d418901a38cb5c7ec1d6de9c207424f6
alegoriyass <alegoriyass@yandex.ru>
2020-06-22 10:42:29 +0300

    Replace models.DateField();

--- Books/book/models.py
+++ Books/book/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Book(models.Model):
     name = models.CharField(max_length=50)
-    pub_date = models.DateField()
+    pub_date = models.DateTimeField()       # models.DateField();  models.DateTimeField(auto_now_add=True, dЬ_index=True)
--- Books/book/views.py
+++ Books/book/views.py
@@ -4,18 +4,5 @@ from .models import Book
 
 def index(request):
     book_list = Book.objects.order_by('-pub_date')#[:10]
-    context = {'book_list':book_list}
-    return render(request, 'book/index.html', context)
+    return render(request, 'book/index.html', {'book_list':book_list})
    
-   #s = 'Список книг\r\n\r\n'
-    #for b in Book.objects.order_by('-pub_date'):
-        #s +=b.name + '\r\n' + str(b.pub_date) + '\r\n\r\n'
-    
-    #return HttpResponse(s, content_type='text/plain; charset=utf-8')
-
-#from django.http import HttpResponse
-#import datetime
-
-#def index(request):
-    #now = datetime.datetime.now()
-   # return HttpResponse(now)
